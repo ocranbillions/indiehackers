5c83a6fee52eaa24cf5344c996ddf88cb8620fd8
Samuel Ocran <sammiestt@gmail.com>
2021-11-26 20:24:03 +0100

    Setup react-jss with theming

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { createUseStyles } from 'react-jss';
 
-const App = () => (
-  <div>
-    <div>app</div>
-  </div>
-);
+const useStyles = createUseStyles((theme) => ({
+  app: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+    background: theme.background,
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.app}>
+      <div>App here</div>
+    </div>
+  );
+};
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'react-jss';
+
 import './index.css';
 import App from './components/App';
 
+const theme = {
+  background: '#0e2439',
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
