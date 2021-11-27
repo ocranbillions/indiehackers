f1aff6d117b2d63fdb6a3c3e1eda3f625c5eb5e8
Samuel Ocran <sammiestt@gmail.com>
2021-11-28 00:08:45 +0100

    Lint

--- src/icons/dropdown-icons/CommunityIcon.jsx
+++ src/icons/dropdown-icons/CommunityIcon.jsx
@@ -1,17 +1,14 @@
-import React from "react"
+import React from 'react';
 
-const CommunityIcon = (props) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      className="prefix__nav-dropdown__icon prefix__ember-view"
-      {...props}
-    >
-      <path d="M20 9.999v-2h4V20h-3v4l-5.333-4H8v-4h12V9.999zM18 14H8.333L3 18v-4H0V-.001h18V14z" fill="currentColor" />
-    </svg>
-  )
-}
+const CommunityIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    className="prefix__nav-dropdown__icon prefix__ember-view"
+    {...props}
+  >
+    <path d="M20 9.999v-2h4V20h-3v4l-5.333-4H8v-4h12V9.999zM18 14H8.333L3 18v-4H0V-.001h18V14z" fill="currentColor" />
+  </svg>
+);
 
 export default CommunityIcon;
-
